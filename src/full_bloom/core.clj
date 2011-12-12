9f1c75a50fd420e64e3cb3f2df2984281b201e68
= <=>
2011-12-12 20:39:58 +1030

    Bloom filter based on compressed bit-sets.

---
+++ src/full_bloom/core.clj
@@ -0,0 +1,1 @@
+(ns full-bloom.core)
---
+++ test/full_bloom/test/core.clj
@@ -0,0 +1,6 @@
+(ns full-bloom.test.core
+  (:use [full-bloom.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
